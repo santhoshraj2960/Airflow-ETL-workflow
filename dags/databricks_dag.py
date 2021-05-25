7fafdd78524e980c18532a8ee1ff3653c2730a2a
SanthoshRaj Murugesan <santhoshraj2960@hotmail.com>
2021-05-25 14:27:32 -0500

    Commenting unused parts of code

--- dags/databricks_dag.py
+++ dags/databricks_dag.py
@@ -22,46 +22,6 @@ args = {
 dag = DAG(dag_id='ETL_newyork_taxi_operator', default_args=args, schedule_interval='@daily')
 cluster_id = None
 
-'''
-new_cluster = {
-    'spark_version': '8.1.x-scala2.12',
-    "node_type_id": "Standard_F4s",
-    'num_workers': 0
-}
-'''
-
-
-def create_databricks_cluster(**kwargs):
-    """
-
-    :return: cluster_id
-    """
-    global cluster_id
-    host = json.loads(BaseHook.get_connection("databricks_default").get_extra())['host']
-    DOMAIN = host.split("https://")[1]
-    TOKEN = json.loads(BaseHook.get_connection("databricks_default").get_extra())['token']
-
-    response = requests.post(
-        'https://%s/api/2.0/clusters/create' % DOMAIN,
-        headers={'Authorization': 'Bearer %s' % TOKEN},
-        json={
-            "cluster_name": "my-cluster",
-            "spark_version": "8.1.x-scala2.12",
-            "node_type_id": "Standard_F4s",
-            "spark_env_vars": {
-                "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
-            },
-            "num_workers": 0,
-            "autotermination_minutes": 30
-        }
-    )
-
-    if response.status_code == 200:
-        print(response.json()['cluster_id'])
-        Variable.set('cluster_id', response.json()['cluster_id'])
-    else:
-        print("Error launching cluster: %s: %s" % (response.json()["error_code"], response.json()["message"]))
-
 
 with dag:
     # Example of using the JSON parameter to initialize the operator.
@@ -105,6 +65,49 @@ with dag:
     mount_storage_accounts >> copy_data_to_delta_lake
     copy_data_to_delta_lake >> [run_etl_yellow_taxi, run_etl_green_taxi, run_etl_fhv_taxi]
     [run_etl_yellow_taxi, run_etl_green_taxi, run_etl_fhv_taxi] >> un_mount_storage
+
+# ------------------------ END OF FILE -------------------------------------------
+
+'''
+new_cluster = {
+    'spark_version': '8.1.x-scala2.12',
+    "node_type_id": "Standard_F4s",
+    'num_workers': 0
+}
+'''
+
+'''
+def create_databricks_cluster(**kwargs):
+    """
+
+    :return: cluster_id
+    """
+    global cluster_id
+    host = json.loads(BaseHook.get_connection("databricks_default").get_extra())['host']
+    DOMAIN = host.split("https://")[1]
+    TOKEN = json.loads(BaseHook.get_connection("databricks_default").get_extra())['token']
+
+    response = requests.post(
+        'https://%s/api/2.0/clusters/create' % DOMAIN,
+        headers={'Authorization': 'Bearer %s' % TOKEN},
+        json={
+            "cluster_name": "my-cluster",
+            "spark_version": "8.1.x-scala2.12",
+            "node_type_id": "Standard_F4s",
+            "spark_env_vars": {
+                "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
+            },
+            "num_workers": 0,
+            "autotermination_minutes": 30
+        }
+    )
+
+    if response.status_code == 200:
+        print(response.json()['cluster_id'])
+        Variable.set('cluster_id', response.json()['cluster_id'])
+    else:
+        print("Error launching cluster: %s: %s" % (response.json()["error_code"], response.json()["message"]))
+'''
 
 '''
     notebook_task = DatabricksSubmitRunOperator(
